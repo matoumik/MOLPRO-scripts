0614ecdb636bd638847a16c4f28b94a7cdff7e96
Mikulas <mikulas@fykos.cz>
2018-01-18 13:28:17 +0100

    [parser.py] added dummy parser function

--- parse.py
+++ parse.py
@@ -2,3 +2,14 @@
 """
 file providing methods for parsing a single molpro out file
 """
+
+import io
+import re
+
+def parse(outfilename):
+    outfile = io.open(outfilename,'r')
+    for textline in outfile:
+        """
+        !TODO
+        """
+    
