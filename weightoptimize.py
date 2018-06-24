d0d32c4de34a42da36e54de373511b34328ed09a
Mikulas <mikulas@fykos.cz>
2018-06-24 02:39:24 +0200

    Bug

--- weightoptimize.py
+++ weightoptimize.py
@@ -80,8 +80,8 @@ class molopt:
             initweights += [1]*st[3]
             bnds += ((0.0,1.0),)*st[3]
         
-        optres = optimize.minimize(self.optfunc, np.array(initweights), method="SLSQP", bounds=bnds,
-                                   options={'disp':True,'ftol':0.01,'eps':0.01})
+        optres = optimize.minimize(self.optfunc, np.array(initweights), method="L-BFGS-B", bounds=bnds,
+                                   options={'disp':True,'ftol':0.01,})
         optweights = optres.x
         outweights = list()
         
